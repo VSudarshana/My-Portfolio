e25ae3b58f0252ea75a56966094071128c4ce378
SUDARSHANA V <140592185+VSudarshana@users.noreply.github.com>
2024-04-09 15:07:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Frontend Developer", "Backend Developer", "Fullstack Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
